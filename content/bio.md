7bef82ec802d704e78d4a0ff6ac238524112a899
Andy Garbett <atgarbett@gmail.com>
2020-07-10 19:29:34 +0100

    Added andy picture and styled bio text

--- content/bio.md
+++ content/bio.md
@@ -1,7 +1,5 @@
 ---
+photo: /images/andy.jpeg
 cv: something.pdf
-
 ---
-![Andy Garbett](/static/andy.jpeg "Andy Garbett")
-
 I'm a User Experience Researcher exploring Human-Centric AI through the design of wellbeing technologies to promote a deeper understanding of ourselves using computer vision, ubiquitous sensing, and personal data.
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,27 @@
 <template lang="pug">
   div
-    section.hero
-      .hero-body
-        .container.has-text-centered
-          h1.title.is-family-primary {{ page.title }}
-          h2.subtitle {{ page.description }}
-    nuxt-content(:document="bio")
-    nuxt-content(:document="page")
+    section
+      .container.has-text-centered
+        h1.title.is-size-3.is-family-primary {{ page.title }}
+        h2.subtitle.is-size-6 {{ page.description }}
+    hr
+    .columns.is-mobile.is-centered
+      .column.is-3-desktop.is-5-tablet.is-8-mobile
+        figure.image
+          img(:src="bio.photo")
+    .columns.is-centered.has-text-centered
+      .column.is-6
+        nuxt-content(:document="bio")
+    .columns.is-centered.has-text-centered
+      .column.is-6
+        a(:href="bio.cv" target="_blank")
+          b-button.button.is-family-secondary.is-primary.is-rounded Curriculum Vitae
+        hr
+
+    section
+      .columns
+        .column.has-text-centered
+          h3.title.is-size-4.is-family-primary Projects
 </template>
 
 <script>
